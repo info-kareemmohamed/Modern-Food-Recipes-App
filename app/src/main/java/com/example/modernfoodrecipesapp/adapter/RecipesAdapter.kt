0f01099edf71f818885069a1b5e0cf5a8ab3e2d3
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-06-24 16:32:56 +0300

    Create RecipesAdapter class

--- app/src/main/java/com/example/modernfoodrecipesapp/adapter/RecipesAdapter.kt
+++ app/src/main/java/com/example/modernfoodrecipesapp/adapter/RecipesAdapter.kt
@@ -1,23 +1,49 @@
 package com.example.modernfoodrecipesapp.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.modernfoodrecipesapp.databinding.RecipesRowLayoutBinding
+import com.example.modernfoodrecipesapp.model.FoodRecipe
+import com.example.modernfoodrecipesapp.model.Result
 
-class RecipesAdapter() :RecyclerView.Adapter<RecipesAdapter.MyViewHolder> (){
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class RecipesAdapter() : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+    private var list = emptyList<Result>()
 
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+
+        fun bind(result: Result) {
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+
+        return MyViewHolder.from(parent)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(list[position])
+    }
+
+    fun setData(newData: FoodRecipe) {
+        list = newData.results
+        notifyDataSetChanged()
     }
 }
