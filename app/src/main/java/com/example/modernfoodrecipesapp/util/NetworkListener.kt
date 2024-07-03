086847ddf2491a15d15fedc2cae73891c03d7059
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-07-03 16:28:42 +0300

    HandleNetworkConnection : Create NetworkListener class

---
+++ app/src/main/java/com/example/modernfoodrecipesapp/util/NetworkListener.kt
@@ -0,0 +1,40 @@
+package com.example.modernfoodrecipesapp.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+
+@ExperimentalCoroutinesApi
+class NetworkListener : ConnectivityManager.NetworkCallback() {
+
+    private val isNetworkAvailable = MutableStateFlow(false)
+
+    fun checkNetworkAvailability(context: Context): MutableStateFlow<Boolean> {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        // Register the network callback
+        connectivityManager.registerDefaultNetworkCallback(this)
+
+        // Check the current network status
+        val network = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+        val isConnected = networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+
+        // Update the network availability state
+        isNetworkAvailable.value = isConnected
+
+        return isNetworkAvailable
+    }
+
+    override fun onAvailable(network: Network) {
+        isNetworkAvailable.value = true
+    }
+
+    override fun onLost(network: Network) {
+        isNetworkAvailable.value = false
+    }
+}
